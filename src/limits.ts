af8cfbe5a44d6eacd510c63f0e1b958ba8993538
Shine <i@nowtime.cc>
2025-03-13 15:04:52 +0800

    feat: 新增相关 qwen 模型；调整默认模型配置

--- src/limits.ts
+++ src/limits.ts
@@ -24,6 +24,49 @@ const modelTokensMap: Record<string, ModelTokens> = {
     maxTokens: 129_024,
     responseTokens: 8192,
   },
+  // -------------------------
+  // 适合复杂任务，推理能力最强
+  'qwen-max': {
+    maxTokens: 30_720,
+    responseTokens: 8192,
+  },
+  'qwen-max-latest': {
+    maxTokens: 30_720,
+    responseTokens: 8192,
+  },
+  // 效果、速度、成本均衡
+  'qwen-plus': {
+    maxTokens: 129_024,
+    responseTokens: 8192,
+  },
+  'qwen-plus-latest': {
+    maxTokens: 129_024,
+    responseTokens: 8192,
+  },
+  // 适合简单任务，速度快、成本极低
+  'qwen-turbo': {
+    maxTokens: 1_000_000,
+    responseTokens: 8192
+  },
+  'qwen-turbo-latest': {
+    maxTokens: 1_000_000,
+    responseTokens: 8192
+  },
+  // 适合大规模文本分析，效果与速度均衡、成本较低
+  'qwen-long': {
+    maxTokens: 1_000_000,
+    responseTokens: 8192
+  },
+  // 多模态输入
+  'qwen-omni-turbo': {
+    maxTokens: 1_000_000,
+    responseTokens: 8192
+  },
+  'qwen-omni-turbo-latest': {
+    maxTokens: 30_720,
+    responseTokens: 2_048
+  },
+  // -------------------------
   'qwen-coder-plus-latest': {
     maxTokens: 129_024,
     responseTokens: 8192,
